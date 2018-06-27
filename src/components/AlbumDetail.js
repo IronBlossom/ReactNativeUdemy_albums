3d075480a60189a69c7998c7c389c942530fb144
Ishtiaq Mahmood Amin <imamin@ael.net>
2018-06-27 22:56:19 +0600

    De-structured 'props' and 'album'

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-    return (
-        <Card>
-            <CardSection>
-                <Text>{props.album.title}</Text>
-            </CardSection>
-        </Card>
-    );
+const AlbumDetail = ({ album: { title, artist, thumbnail_image } }) => (
+    <Card>
+        <CardSection>
+            <View>
+                <Image source={{ uri: thumbnail_image }} />
+            </View>
+            <View style={styles.headerContentStyle}>
+                <Text>{title}</Text>
+                <Text>{artist}</Text>
+            </View>
+        </CardSection>
+    </Card>
+);
+
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
 };
+
 
 export default AlbumDetail;
