4ca48a3533a56b03ab6fdbe232eb6e87e12842a4
Ishtiaq Mahmood Amin <ironblossom@Ishtiaqs-Mac-mini.local>
2018-07-01 17:54:29 +0600

    De-structre 'album' in another way

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,19 +3,22 @@ import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = ({ album: { title, artist, thumbnail_image } }) => (
-    <Card>
-        <CardSection>
-            <View>
-                <Image source={{ uri: thumbnail_image }} />
-            </View>
-            <View style={styles.headerContentStyle}>
-                <Text>{title}</Text>
-                <Text>{artist}</Text>
-            </View>
-        </CardSection>
-    </Card>
-);
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    return (
+        <Card>
+            <CardSection>
+                <View>
+                    <Image source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+        </Card>
+    );
+};
 
 
 const styles = {
